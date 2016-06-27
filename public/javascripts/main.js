c2c9ad98f2ba5eea3c90603253936511f365f443
sulvto <lpetrel@outlook.com>
2016-06-27 23:46:01 +0800

    合并 Offerer 和 Answerer 对象

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -43,14 +43,14 @@ signalingChannel.on("RTCDataChannel", function (data) {
         if (data.offerSdp) {
             if (!window.answerer[data.from]) {
                 console.log("====offerSdp====" + nowDate.toLocaleTimeString() + " " + nowDate.getMilliseconds(), window.answerer);
-                window.answerer[data.from] = {peer: Answerer.createAnswer(data.offerSdp, data.from)};
+                window.answerer[data.from] = WebRTCPeerConnection.createAnswer(data.from, data.offerSdp);
             }
         } else
         // if other user created answer; and sent you answer-sdp
         if (data.answerSdp) {
             if (window.offerer[data.from]) {
                 console.log("====answerSdp====" + nowDate.toLocaleTimeString() + " " + nowDate.getMilliseconds(), window.offerer);
-                window.offerer[data.from].peer.setRemoteDescription(data.answerSdp);
+                window.offerer[data.from].setRemoteDescription(data.answerSdp);
             }
         } else
         // if other user sent you ice candidates
@@ -58,10 +58,10 @@ signalingChannel.on("RTCDataChannel", function (data) {
             // it will be fired both for offerer and answerer
             if (window.answerer[data.from]) {
                 console.log("====ice answerer====" + nowDate.toLocaleTimeString() + " " + nowDate.getMilliseconds(), window.answerer);
-                window.answerer[data.from].peer.addIceCandidate(data.ice);
+                window.answerer[data.from].addIceCandidate(data.ice);
             } else if (window.offerer[data.from]) {
                 console.log("====ice offerer====" + nowDate.toLocaleTimeString() + " " + nowDate.getMilliseconds(), window.offerer);
-                window.offerer[data.from].peer.addIceCandidate(data.ice);
+                window.offerer[data.from].addIceCandidate(data.ice);
             }
         } else {
 
@@ -69,7 +69,7 @@ signalingChannel.on("RTCDataChannel", function (data) {
     } else if (!data.target) {
         if (!window.answerer[data.from]) {
             console.log("====  createOffer   ====" + nowDate.toLocaleTimeString() + " " + nowDate.getMilliseconds());
-            window.offerer[data.from] = {peer: Offerer.createOffer(data.from)};
+            window.offerer[data.from] = WebRTCPeerConnection.createOffer(data.from);
         }
     }
 });
@@ -89,8 +89,9 @@ function failureCallback(data) {
     //TODO error
     console.log(data);
 }
-var Offerer = {
-    createOffer: function (target) {
+
+var WebRTCPeerConnection = {
+    connection: function (target) {
         var peer = new PeerConnection(iceServers);
         peer.addStream(window.localMediaStream);
 
@@ -102,100 +103,88 @@ var Offerer = {
             }
         };
 
+        peer.onaddstream = function (event) {
+            console.log("peer.onaddstream", event);
+            attachStream(event.stream, target)
+        };
+
+        peer.onremovestream = function (event) {
+            console.log("peer.onremovestream", event);
+        };
+
         // let the "negotiationneeded" event trigger offer generation
         peer.onnegotiationneeded = function () {
             console.log("peer.onnegotiationneeded");
         };
 
-        var offererDataChannel = peer.createDataChannel('RTCDataChannel', moz ? {} : {
-            reliable: false // Deprecated
-        });
-        if (moz) {
-            offererDataChannel.binaryType = 'blob';
-        }
-        setChannelEvents(offererDataChannel);
-        peer.createOffer(function (sdp) {
-            peer.setLocalDescription(sdp);
-            SOCKET.sendOfferSdp(sdp, target);
-        }, failureCallback);
-        this.peer = peer;
-        return this;
-    }, setRemoteDescription: function (sdp) {
-        this.peer.setRemoteDescription(new SessionDescription(sdp));
-    },
-    addIceCandidate: function (candidate) {
-        this.peer.addIceCandidate(new IceCandidate({
-            sdpMLineIndex: candidate.sdpMLineIndex,
-            candidate: candidate.candidate
-        }));
-    }
-}
-
-var Answerer = {
-    createAnswer: function (offerSDP, target) {
-        var peer = new PeerConnection(iceServers);
-        peer.addStream(window.localMediaStream);
-
+        // answer
         peer.ondatachannel = function (event) {
             var answererDataChannel = event.channel;
             if (moz) {
                 answererDataChannel.binaryType = 'blob';
             }
-
-            setChannelEvents(answererDataChannel);
+            this.setChannelEvents(answererDataChannel);
         };
 
-        peer.onaddstream = function (event) {
-            console.log("channel.onaddstream", event);
-            attachStream(event.stream, target)
+        return peer;
+    }, setChannelEvents: function (channel) {
+        channel.onmessage = function (event) {
+            console.log('WebRTC DataChannel onmessage', event);
+            onMsg(event.data);
         };
 
-        peer.onicecandidate = function (event) {
-            if (event.candidate) {
-                SOCKET.sendIce(event.candidate, target);
-            }
+        channel.onopen = function () {
+            console.log(channel);
+            RTCDataChannels[RTCDataChannels.length] = channel;
+
+            //TODO
+            console.log("====ONOPEN====ONOPEN====ONOPEN====ONOPEN====ONOPEN====");
         };
 
-        peer.setRemoteDescription(new SessionDescription(offerSDP));
-
-        peer.createAnswer(function (sdp) {
-            peer.setLocalDescription(sdp);
-            SOCKET.sendAnswerSdp(sdp, target);
-        }, failureCallback);
-
-
-        this.peer = peer;
-        return this;
-    },
-    addIceCandidate: function (candidate) {
+        channel.onclose = function (event) {
+            console.warn('WebRTC DataChannel closed', event);
+        };
+        channel.onerror = function (event) {
+            console.error('WebRTC DataChannel error', event);
+        };
+    }, setRemoteDescription: function (sdp) {
+        this.peer.setRemoteDescription(new SessionDescription(sdp));
+    }, addIceCandidate: function (candidate) {
         this.peer.addIceCandidate(new IceCandidate({
             sdpMLineIndex: candidate.sdpMLineIndex,
             candidate: candidate.candidate
         }));
+    }, createDataChannel: function () {
+        var offererDataChannel = this.peer.createDataChannel('RTCDataChannel', moz ? {} : {
+            reliable: false // Deprecated
+        });
+        if (moz) {
+            offererDataChannel.binaryType = 'blob';
+        }
+        this.setChannelEvents(offererDataChannel);
+    }, createOffer: function (target) {
+        this.peer = this.connection(target);
+        var that = this;
+        this.createDataChannel();
+
+        this.peer.createOffer(function (sdp) {
+            that.peer.setLocalDescription(sdp);
+            SOCKET.sendOfferSdp(sdp, target);
+        }, failureCallback);
+        return this;
+    }, createAnswer: function (target, offerSDP) {
+        this.peer = this.connection(target);
+        var that = this;
+        this.setRemoteDescription(offerSDP);
+
+        this.peer.createAnswer(function (sdp) {
+            that.peer.setLocalDescription(sdp);
+            SOCKET.sendAnswerSdp(sdp, target);
+        }, failureCallback);
+        return this;
     }
 };
 
-function setChannelEvents(channel) {
-    channel.onmessage = function (event) {
-        console.log('WebRTC DataChannel onmessage', event);
-        onMsg(event.data);
-    };
-
-    channel.onopen = function () {
-        console.log(channel);
-        RTCDataChannels[RTCDataChannels.length] = channel;
-
-        //TODO
-        console.log("====ONOPEN====ONOPEN====ONOPEN====ONOPEN====ONOPEN====");
-    };
-
-    channel.onclose = function (event) {
-        console.warn('WebRTC DataChannel closed', event);
-    };
-    channel.onerror = function (event) {
-        console.error('WebRTC DataChannel error', event);
-    };
-}
 
 function attachStream(stream, id) {
     var newVideo = document.createElement("video");
@@ -264,14 +253,16 @@ function sendMsg() {
 
 window.setInterval(function () {
     console.log("RTCDataChannels :: " + RTCDataChannels.length);
-}, 10000);
+    console.log("   offerer:", window.offerer);
+    console.log("   answerer:", window.answerer);
+}, 5000);
 
 
 userMedia.call(navigator, {video: true, audio: true},
     function (stream) {
         window.localMediaStream = stream;
-        //document.getElementById('me').src = URL.createObjectURL(stream);
-        //document.getElementById('me').play();
+        document.getElementById('me').src = URL.createObjectURL(stream);
+        document.getElementById('me').play();
     },
     function (error) {
         console.log(error);
